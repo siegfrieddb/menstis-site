474e6e2b1581098b6b94c98b8b48475b187f1698
sylvaindb <sylvain@menstis.be>
2018-02-04 22:13:27 +0100

    workaround for missing video in blog

--- start bouw.bat
+++ start bouw.bat
@@ -1,2 +1,8 @@
 rmdir /s /q .\public 
+md .\public
+md .\public\blog
+md .\public\blog\2015-01-04-Archief20Jaar
+md .\public\blog\2015-01-04-Archief20Jaar\movie
+copy .\pages\blog\2015-01-04-Archief20Jaar\menstis.m4v .\public\blog\2015-01-04-Archief20Jaar\movie\menstis.m4v
 gatsby build
+
