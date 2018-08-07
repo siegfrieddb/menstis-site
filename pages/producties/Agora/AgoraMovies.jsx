1f68c0215b44acc0c4273995b2d5835a27aeb7b7
sylvaindb <sylvain@menstis.be>
2018-08-07 23:15:28 +0200

    agora filmpje pilgrilmage

--- pages/producties/Agora/AgoraMovies.jsx
+++ pages/producties/Agora/AgoraMovies.jsx
@@ -27,20 +27,23 @@ const FilmFocus = React.createClass({
   
 
   onImageClick(e, el) {
-    browserHistory.push(prefixLink(el))
+    console.log(el);
+    
+    browserHistory.push(el)
   },
   render () {
     var movies = []
     const movieLinks = []
     const movieTitles = []
+    const movieImageFile = []
     
-    movieLinks.push("https://www.youtube.com/embed/zG_OGGv9NZ0")
+    movieLinks.push("https://www.youtube.com/embed/zG_OGGv9NZ0?rel=0;&autoplay=1")
     movieTitles.push("PilgrImage")
-    
+    movieImageFile.push("./PilgrImage.jpg")
     
     
 
-
+    var that = this;        
     var gridMovies = []
     if (movieLinks.length ==0 )
     {
@@ -52,15 +55,29 @@ const FilmFocus = React.createClass({
             
             var line = movieLinks.splice(0,1);
             line.forEach( (el, idx) => {
-            gridMovies.push((               
-            <div className="column"> 
-            <iframe style={{"margin-bottom": "-10px" }} frameborder="0" allowfullscreen width="360" height="200" src= {el+"?hl=en_US&amp;fs=0&amp;rel=0&amp;hd=0&amp;loop=1&amp;showinfo=0&amp;cc_load_policy=0&amp;showsearch=0"} ></iframe>
-            <div style={{"font-size": "small"}}>{movieTitles[i]}</div>
+            gridMovies.push((  <div className="column">
+              {/*with no tv url but just embedded
+              
+             
+            <iframe style={{"border": "none" ,"margin-bottom": "-10px" }} frameborder="0" allowfullscreen width="360" height="200" src= {el+"?hl=en_US&amp;fs=0&amp;rel=0&amp;hd=0&amp;loop=1&amp;showinfo=0&amp;cc_load_policy=0&amp;showsearch=0"} ></iframe>
+            <div style={{"paddingTop":"1 el", "font-size": "small"}}>{movieTitles[i]}</div>
+              */}
+              {
+                <div className="image-fit" >
+                <a href={el} id={"extimg"+i} >
+                  <Image  src={prefixLink(movieImageFile[i])}  />
+                  <div className="image-overlay">
+                    <div className="image-text">{movieTitles[i]}</div>
+                  </div>
+                </a>
+                </div>
+              }
+
             {/*
             <iframe frameborder="0" allowfullscreen width="350" height="200" src= {el+"?hl=en_US&amp;fs=0&amp;rel=0&amp;hd=0&amp;loop=1&amp;showinfo=0&amp;cc_load_policy=0&amp;showsearch=0"} ></iframe>
             <iframe width="350" height="200" src= {el} frameborder="0" allowfullscreen></iframe>*/}
             {/*
-            <iframe width="350" height="200" src="https://www.youtube.com/embed/eHkUbYKncCE?modestbranding=1&amp;title=test&amp;rel=0&amp;controls=0&amp;" frameborder="0" allowfullscreen></iframe>
+            <iframe width="350" height="200" src="https://gf www.youtube.com/embed/eHkUbYKncCE?modestbranding=1&amp;title=test&amp;rel=0&amp;controls=0&amp;" frameborder="0" allowfullscreen></iframe>           
             <iframe width="350" height="200" src="https://www.youtube.com/embed/eHkUbYKncCE?modestbranding=1&amp;title=test&amp;rel=0&amp;controls=0&amp;" frameborder="0" allowfullscreen></iframe>
             <iframe
                 src={el} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
