c048ce2d3d3b0d1a5d24b0dd94c583158e394cbd
sylvain <sylvain@mentis.be>
2020-04-04 13:16:07 +0200

    lanceer en deca

--- Lanceer online.bat
+++ Lanceer online.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\WinSCP\WinSCP.exe" /log="C:\menstis-site\log\WinSCP.log" /ini=nul /script="C:\menstis-site\winSCPScript.txt
+"C:\Users\Sylvain\AppData\Local\Programs\WinSCP\WinSCP.com" /log="C:\menstis-site\log\WinSCP.log" /ini=nul /script="C:\menstis-site\winSCPScript.txt
